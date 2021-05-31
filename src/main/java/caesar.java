b353ab02c8677bad8d680193709628de953f2360
Moringa School <abdallah.abdul@student.moringaschool.com>
2021-05-31 14:48:30 +0300

    add the changes made

--- src/main/java/caesar.java
+++ src/main/java/caesar.java
@@ -3,22 +3,22 @@ import org.graalvm.compiler.debug.TTY;
 import java.util.Scanner;
 public class caesar{
     public static void main(String[] args) {
-        System.out.println("welcome to the caesar cipher");
+        System.out.println("welcome to the caesar_cipher App");
         while (true) {
-            System.out.println("take e For encryption or D for decryption");
+            System.out.println("Chose 'E' For encryption or D for decryption");
             Scanner input = new Scanner(System.in);
             String choice = input.nextLine().toLowerCase();
             if (choice.equals("e")) {
-                System.out.println("Enter Test For Encryption");
+                System.out.println("Input Test For Encryption");
                 String planeText = input.nextLine().toLowerCase();
-                System.out.println("Enter a random value");
+                System.out.println("Input a random value");
                 int num = input.nextInt();
                 App message = new App(planeText, num);
                 System.out.println("plane Text:" + message.getMessage() + "/n new Value of:" + message.getNum());
                 System.out.println("Encrypt Test:" + message.encryption());
                 break;
             } else if (choice.equals("d")) {
-                System.out.println("Enter Test to Decrypt:");
+                System.out.println("Input Test to Decrypt:");
                 String encryptedText = input.nextLine().toLowerCase();
                 System.out.println("Enter numValue");
                 int reversValue = input.nextInt();
