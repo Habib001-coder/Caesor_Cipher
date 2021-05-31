ed05ae5fafcbbfe6cf49e390c270a0dd3a57bd4e
Moringa School <abdallah.abdul@student.moringaschool.com>
2021-05-31 12:59:52 +0300

    add the changes made

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,51 @@
+
+
 public class App {
 
+    public String message;
+    public int num;
+    public App(String message, int num) {
+        this.message = message;
+        this.num = num;
+    }
+    public String getMessage() {
+        return message;
+    }
+    public int getNum() {
+        return num;
+    }
+    public String encryption(){
+        StringBuilder sb= new StringBuilder();
+        for( char character : message.toCharArray()){
+            if( character != ' '){
+                int asciiPosition = character -'a';
+                int newPosition = (asciiPosition + num)%26;
+                char newCharacter = (char)('a' + newPosition);
+                sb.append(newCharacter);
+            }else{
+                sb.append(character);
+            }
+        }
+        return sb.toString();
+    }
+    public String decryption(){
+        int reverse = 26 - (num % 26);
+        StringBuilder sb = new StringBuilder();
+        for (char character : message.toCharArray()){
+            if( character != ' ') {
+                int asciiPosition = character - 'a';
+                int newPosition = (asciiPosition + reverse) % 26;
+                char newCharacter = (char) ('a' + newPosition);
+                sb.append(newCharacter);
+            }
+            else{
+                sb.append(character);
+            }
+        }
+        return sb.toString();
+    }
 }
+
+
+
+
--- src/main/java/caesar.java
+++ src/main/java/caesar.java
@@ -1,40 +1,33 @@
-//import org.graalvm.compiler.debug.TTY;
-//
-//import java.util.Scanner;
-//
-//public class caesar {
-//    public static void main(String[] args) {
-//        Scanner aScanner =  new Scanner(System.in);
-//        String pLainText;
-//        int shiftKey;
-//        System.out.println("Please enter the message to be encrypted: ");
-//        pLainText = aScanner.nextLine();
-//        System.out.println("Please enter the shift key ");
-//        shiftKey = aScanner.nextInt();
-//
-//
-////        sytem.out.prIntIn("The Encrypted Text:"+EncryptedText(pLainText,shiftKey));
-//    }
-//
-//public static  String cyperText(String message, int shiftKey) {
-////    c(x) = x + shiftKey Mod(%) 26 ;
-//    final String ALPHABETS = "abcdefghijklmnopqrstuvwxyz";
-//    message = message.toLowerCase();
-//
-//    StringBuilder cypherText = new StringBuilder();
-//    int counter;
-//    for (counter = 0; counter < message.length(); counter++) ;
-//    if (!Character.isLetter(message.charAt(counter))) {
-//
-//        cypherText.append(message.charAt(counter));
-//
-//
-//    else int charPosition = ALPHABETS.indexOf(message.charAt(counter));
-//        char typeValue = ALPHABETS.charAt(keyValue);
-//        cypherText.append(cyperValue);
-//    }
-//}
-//return cyperText.toString();
-//        }
+import org.graalvm.compiler.debug.TTY;
 
-
+import java.util.Scanner;
+public class caesar{
+    public static void main(String[] args) {
+        System.out.println("welcome to the caesar cipher");
+        while (true) {
+            System.out.println("take e For encryption or D for decryption");
+            Scanner input = new Scanner(System.in);
+            String choice = input.nextLine().toLowerCase();
+            if (choice.equals("e")) {
+                System.out.println("Enter Test For Encryption");
+                String planeText = input.nextLine().toLowerCase();
+                System.out.println("Enter a random value");
+                int num = input.nextInt();
+                App message = new App(planeText, num);
+                System.out.println("plane Text:" + message.getMessage() + "/n new Value of:" + message.getNum());
+                System.out.println("Encrypt Test:" + message.encryption());
+                break;
+            } else if (choice.equals("d")) {
+                System.out.println("Enter Test to Decrypt:");
+                String encryptedText = input.nextLine().toLowerCase();
+                System.out.println("Enter numValue");
+                int reversValue = input.nextInt();
+                App encryptText = new App(encryptedText, reversValue);
+                System.out.println("decrypted message: " + encryptText.decryption());
+                break;
+            } else {
+                System.out.println("invalid choice");
+            }
+        }
+    }
+}
