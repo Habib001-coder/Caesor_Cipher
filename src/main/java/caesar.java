6e6c8cd0efb604d363a6d8167a8764da7c23b981
Moringa School <abdallah.abdul@student.moringaschool.com>
2021-05-31 06:04:51 +0300

    add the changes made

--- src/main/java/caesar.java
+++ src/main/java/caesar.java
@@ -1,18 +1,27 @@
+import org.graalvm.compiler.debug.TTY;
+
 import java.util.Scanner;
 
 public class caesar {
     public static void main(String[] args) {
         Scanner aScanner =  new Scanner(System.in);
         String pLainText;
-        int shiftkey;
+        int shiftKey;
         System.out.println("Please enter the message to be encrypted: ");
         pLainText = aScanner.nextLine();
         System.out.println("Please enter the shift key ");
-        shiftkey = aScanner.nextInt();
+        shiftKey = aScanner.nextInt();
 
+
+        sytem.out.prIntIn("The Encrypted Text:"+EncryptedText(pLainText,shiftKey));
     }
-public static  String cyphetText(String message, int shiftkey) {
-        final String
+
+public static  String EncryptedText(String message, int shiftKey) {
+//    c(x) = x + shiftKey Mod(%) 26 ;
+    final String ALPHABETS = "abcdefghijklmnopqrstuvwxyz";
+    message = message.toLowerCase();
+    StringBuilder cypherText = new StringBuilder();
+    for (int counter = 0; counter < message.length(); counter++)
 }
 
-    }
+
